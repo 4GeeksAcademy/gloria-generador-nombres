4133a3b3a0d5b3019a7830681daa4b1bfe094d07
Gloria Albino <gloriaalbino.gg@gmail.com>
2024-11-27 05:03:28 +0000

    css de generador de nombre

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Generador de dominio
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#nombres").innerHTML = nombres();
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#nombres").innerHTML = nombres();
+  });
 };
+
+function nombres() {
+  let pronouns = ["I", "you", "he", "she", "it", "we", "they"];
+  let adjetives = [
+    "happy",
+    "sad",
+    "strong",
+    "beautiful",
+    "fast",
+    "smart",
+    "angry"
+  ];
+  let nouns = ["dog", "cat", "pencil", "phone", "glass", "teacher", "computer"];
+  let extensions = [".com", ".net", ".us", ".io", ".org"];
+
+  let pronombre = Math.floor(Math.random() * pronouns.length);
+  let adjetivos = Math.floor(Math.random() * adjetives.length);
+  let sustantivos = Math.floor(Math.random() * nouns.length);
+  let extensiones = Math.floor(Math.random() * extensions.length);
+
+  return (
+    pronouns[pronombre] +
+    adjetives[adjetivos] +
+    nouns[sustantivos] +
+    extensions[extensiones]
+  );
+}
