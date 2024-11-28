586282b0f95127a1187a89443351a94daf67f1eb
Gloria Albino <gloriaalbino.gg@gmail.com>
2024-11-28 02:36:46 +0000

    se colocaron todos las combinaciones posibles

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import "./assets/img/4geeks.ico";
 // Generador de dominio
 window.onload = function() {
   document.querySelector("#nombres").innerHTML = nombres();
+  document.getElementById("nombres").innerHTML = nombres();
   document.querySelector("#boton").addEventListener("click", () => {
     document.querySelector("#nombres").innerHTML = nombres();
   });
@@ -25,16 +26,17 @@ function nombres() {
   ];
   let nouns = ["dog", "cat", "pencil", "phone", "glass", "teacher", "computer"];
   let extensions = [".com", ".net", ".us", ".io", ".org"];
+  let dominios = "";
 
-  let pronombre = Math.floor(Math.random() * pronouns.length);
-  let adjetivos = Math.floor(Math.random() * adjetives.length);
-  let sustantivos = Math.floor(Math.random() * nouns.length);
-  let extensiones = Math.floor(Math.random() * extensions.length);
-
-  return (
-    pronouns[pronombre] +
-    adjetives[adjetivos] +
-    nouns[sustantivos] +
-    extensions[extensiones]
-  );
+  for (let i = 0; i < pronouns.length; i++) {
+    for (let j = 0; j < adjetives.length; j++) {
+      for (let k = 0; k < nouns.length; k++) {
+        for (let l = 0; l < extensions.length; l++) {
+          dominios += `<h6> ${pronouns[i]}${pronouns[j]}${nouns[k]}${extensions[l]} </h6>`;
+          console.log(pronouns[i] + adjetives[j] + nouns[k] + extensions[l]);
+        }
+      }
+    }
+  }
+  return dominios;
 }
